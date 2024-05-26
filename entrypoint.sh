a501592b1910d3d563c952510862d34eb0870ef8
esculapeso <103459392+esculapeso@users.noreply.github.com>
2024-05-26 19:33:29 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,5 +18,6 @@ exec ./build/bin/geth \
   --http.corsdomain "*" \
   --ipcpath /root/.esa/geth.ipc \
   --datadir /root/.esa \
+  --networkid 83278 \
   ${IP:+--nat extip:"$IP"} \
   ${BOOTNODES:+--bootnodes "$BOOTNODES"}
