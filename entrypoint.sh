3227226b943517bc702aa4a734b4db5104d444fa
esculapeso <103459392+esculapeso@users.noreply.github.com>
2024-05-28 12:02:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Starting Geth with the following parameters:"
+echo "IP: $IP"
+echo "BOOTNODES: $BOOTNODES"
+
 # Initialize the Geth node with the genesis file
 ./build/bin/geth --datadir=/root/.esa init /root/core-geth/esa_genesis.json
 
@@ -10,7 +14,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Start the Geth node with the specified parameters
-exec ./build/bin/geth \
+GETH_CMD="./build/bin/geth \
   --http \
   --http.addr "0.0.0.0" \
   --http.port 8545 \
@@ -21,4 +25,8 @@ exec ./build/bin/geth \
   --allow-insecure-unlock \
   --networkid 83278 \
   ${IP:+--nat extip:"$IP"} \
-  ${BOOTNODES:+--bootnodes "$BOOTNODES"}
+  ${BOOTNODES:+--bootnodes "$BOOTNODES"}"
+
+echo "Executing: $GETH_CMD"
+
+exec $GETH_CMD
