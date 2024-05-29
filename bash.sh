4ea449ec50bfe05aab4656caf3e7d394d3fb8c0c
esculapeso <103459392+esculapeso@users.noreply.github.com>
2024-05-29 09:51:39 +0200

    Update bash.sh command preview

--- bash.sh
+++ bash.sh
@@ -9,7 +9,9 @@ esacoin() {
         if [ "$2" = "gai" ]; then
             docker run –name esanode -d -p 8546:8546 -p 30303:30303 -v ${this_root_path}:/root/.esa -e IP=$this_ip -e esculapeso/core-geth:latest
         else
-            docker run –name esanode -d -p 8546:8546 -p 30303:30303 -v ${this_root_path}:/root/.esa -e IP=$this_ip -e BOOTNODES=$gai_bootnode esculapeso/core-geth:latest
+            local cmd="docker run –name esanode -d -p 8546:8546 -p 30303:30303 -v ${this_root_path}:/root/.esa -e IP=$this_ip -e BOOTNODES=$gai_bootnode esculapeso/core-geth:latest"
+            echo "Preview: $cmd"
+            eval $cmd
         fi
     fi
 }
