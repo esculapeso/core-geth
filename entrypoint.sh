9ca1518a9a8e115e8761963a55ba6882812b91c3
esculapeso <103459392+esculapeso@users.noreply.github.com>
2024-06-02 21:01:29 +0200

    updated balances

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,9 +70,9 @@ if [ "$FIRST_NODE" = "true" ] && [ ! -f "$FLAG_FILE" ]; then
   echo "New account addresses: $ACCOUNT_ADDRESS_1, $ACCOUNT_ADDRESS_2, $ACCOUNT_ADDRESS_3"
 
   # Update the genesis.json file with the new account addresses and balances
-  jq --arg address1 "$ACCOUNT_ADDRESS_1" --arg balance1 "0x3C3B6C0" \
-     --arg address2 "$ACCOUNT_ADDRESS_2" --arg balance2 "0x503EAC0" \
-     --arg address3 "$ACCOUNT_ADDRESS_3" --arg balance3 "0x3C3B6C0" \
+  jq --arg address1 "$ACCOUNT_ADDRESS_1" --arg balance1 "0x3C14DC0" \
+     --arg address2 "$ACCOUNT_ADDRESS_2" --arg balance2 "0x501BD00" \
+     --arg address3 "$ACCOUNT_ADDRESS_3" --arg balance3 "0x3C14DC0" \
      '.alloc[$address1] = { "balance": $balance1 } |
       .alloc[$address2] = { "balance": $balance2 } |
       .alloc[$address3] = { "balance": $balance3 }' \
